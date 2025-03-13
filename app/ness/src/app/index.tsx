eaf2d31c8166807b663b266ee62830fb7ca66b9a
junsu-newid <junsu@its-newid.com>
2025-03-13 15:47:49 +0900

    :test_tube: 콘솔을 추가했네3

--- app/ness/src/app/index.tsx
+++ app/ness/src/app/index.tsx
@@ -4,6 +4,7 @@ import { AppRouter } from './router';
 export const App = () => {
     console.log('test');
     console.log('test2');
+    console.log('test3');
     return (
         <AppProvider>
             <AppRouter />
