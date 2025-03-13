6c2a7dd9e726b9c925e1f295217a10c17005fbdf
junsu-newid <junsu@its-newid.com>
2025-03-13 16:38:44 +0900

    :test_tube: 마지막 인사

--- app/ness/src/App.tsx
+++ app/ness/src/App.tsx
@@ -5,7 +5,7 @@ import { CommonButton } from '@plitvice/ui';
 function App() {
     const { t } = useTranslation();
     console.log('거짓말');
-    console.log('');
+    console.log('마지막 인사');
     console.log('');
     console.log('');
     console.log('');
