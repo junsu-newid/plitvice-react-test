979a3978432614f9843cdec2a260f7c38a0860f0
junsu-newid <junsu@its-newid.com>
2025-07-11 13:35:22 +0900

    :construction: 333333333

--- app/paro/src/App.tsx
+++ app/paro/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     const { t } = useTranslation();
     // 11111111111
     // 22222222222
+    // 33333333333
 
     useEffect(() => {
         fetchLegal('US').then((result) => {
