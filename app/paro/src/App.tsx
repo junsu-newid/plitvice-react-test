4071b50b7a29f00d0613aa70578a8c5c3efa5c33
junsu-newid <135789016+junsu-newid@users.noreply.github.com>
2025-07-11 13:39:27 +0900

    Revert "Test/test commit (#16)" (#17)
    This reverts commit 02352ae7b2f3d9aed07b6cabf5c1ffe8ede3a075.

--- app/paro/src/App.tsx
+++ app/paro/src/App.tsx
@@ -6,9 +6,6 @@ import { fetchLegal } from '@/api/service/legal.ts';
 
 function App() {
     const { t } = useTranslation();
-    // 11111111111
-    // 22222222222
-    // 33333333333
 
     useEffect(() => {
         fetchLegal('US').then((result) => {
